12cadbdb4ec3b6af42318c6d142f0c7b319686ca
Piotr Buda <pibuda@gmail.com>
2012-08-07 14:01:33 +0200

    Running git flow init in a background task so it doesn't block the UI

--- src/com/github/pbuda/gitflowidea/action/GitFlowInitAction.java
+++ src/com/github/pbuda/gitflowidea/action/GitFlowInitAction.java
@@ -19,8 +19,8 @@ package com.github.pbuda.gitflowidea.action;
 import com.github.pbuda.gitflowidea.*;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.progress.*;
+import com.intellij.openapi.progress.Task.*;
 import com.intellij.openapi.project.*;
-import com.intellij.openapi.vfs.*;
 import git4idea.actions.*;
 import git4idea.repo.*;
 import org.jetbrains.annotations.*;
@@ -30,25 +30,26 @@ import org.jetbrains.annotations.*;
  */
 public class GitFlowInitAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
-        GitFlow gitFlow = new GitFlow();
-        Project project = e.getProject();
-        VirtualFile baseDir = project.getBaseDir();
-        int exitCode = gitFlow.initDefault(project, baseDir);
-        if (exitCode != 1) {
-            throw new IllegalStateException();
-        }
-        refreshVcsChanges(e.getProject(), baseDir);
-    }
-
-    private void refreshVcsChanges(final Project project, final VirtualFile baseDir) {
-        new Task.Backgroundable(project, "Refreshing") {
-            public void run(@NotNull ProgressIndicator indicator) {
-                GitInit.refreshAndConfigureVcsMappings(project, baseDir, "");
-                GitRepositoryManager repositoryManager = project.getComponent(GitRepositoryManager.class);
-                for (GitRepository repository : repositoryManager.getRepositories()) {
-                    repository.getUntrackedFilesHolder().invalidate();
-                }
+        final Project finalProject = e.getProject();
+        new Backgroundable(finalProject, "Initializing git flow") {
+            @Override
+            public void run(@NotNull ProgressIndicator progressIndicator) {
+                runInit(finalProject);
+                refreshAfterInit(finalProject);
             }
         }.queue();
+    }
+
+    private void runInit(Project project) {
+        GitFlow gitFlow = new GitFlow();
+        gitFlow.initDefault(project, project.getBaseDir());
+    }
+
+    private void refreshAfterInit(final Project project) {
+        GitInit.refreshAndConfigureVcsMappings(project, project.getBaseDir(), "");
+        GitRepositoryManager repositoryManager = project.getComponent(GitRepositoryManager.class);
+        for (GitRepository repository : repositoryManager.getRepositories()) {
+            repository.getUntrackedFilesHolder().invalidate();
+        }
     }
 }
